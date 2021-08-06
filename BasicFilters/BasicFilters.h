4c4ed68ea4faa42b8be0968803c7b29f9d61c82c
aygunoglu <hasanaygunoglu@gmail.com>
2021-08-06 17:59:28 +0300

    Initial Commit

---
+++ BasicFilters/BasicFilters.h
@@ -0,0 +1,18 @@
+//
+//  BasicFilters.h
+//  BasicFilters
+//
+//  Created by Hasan Aygünoglu on 6.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasicFilters.
+FOUNDATION_EXPORT double BasicFiltersVersionNumber;
+
+//! Project version string for BasicFilters.
+FOUNDATION_EXPORT const unsigned char BasicFiltersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicFilters/PublicHeader.h>
+
+
